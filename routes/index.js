325da26ae595401e58d5c5a0353e11cb137a39f8
Marcin Skic <marcinskicoficjal@gmail.com>
2023-05-05 11:45:03 +0200

    Create controllers and router, finish tut part 4

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-    res.render("index", { title: "Express" });
+    res.redirect("/catalog");
+});
+
+router.get("/loop", function (req, res) {
+    res.redirect("/loop2");
+});
+
+router.get("/loop2", function (req, res) {
+    res.redirect("/loop");
 });
 
 module.exports = router;
