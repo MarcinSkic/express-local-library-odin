a2db16c4c06faf5907070050d647d21663fa5d7c
Marcin Skic <marcinskicoficjal@gmail.com>
2023-05-21 16:12:34 +0200

    Create read-only views, finish tut part 5

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 const catalogRouter = require("./routes/catalog");
 
 const app = express();
@@ -20,7 +19,7 @@ async function main() {
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set("view engine", "pug");
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -29,7 +28,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/catalog", catalogRouter);
 
 // catch 404 and forward to error handler
--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,12 +1,32 @@
 const Author = require("../models/author");
+const Book = require("../models/book");
 const asyncHandler = require("express-async-handler");
 
 exports.author_list = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Author list");
+    const authors = await Author.find().sort({ family_name: 1 }).exec();
+    res.render("author_list", {
+        title: "Author List",
+        author_list: authors,
+    });
 });
 
 exports.author_detail = asyncHandler(async (req, res, next) => {
-    res.send(`NOT IMPLEMENTED: Author detail ${req.params.id}`);
+    const [author, books] = await Promise.all([
+        Author.findById(req.params.id).exec(),
+        Book.find({ author: req.params.id }, "title summary").exec(),
+    ]);
+
+    if (author === null) {
+        const err = new Error("Author not found.");
+        err.status = 404;
+        return next(err);
+    }
+
+    res.render("author_detail", {
+        title: "Author Detail",
+        author: author,
+        author_books: books,
+    });
 });
 
 exports.author_create_get = asyncHandler(async (req, res, next) => {
