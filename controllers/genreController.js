037aff2e6431db98051807df84234c7e07e20453
Marcin Skic <marcinskicoficjal@gmail.com>
2023-05-22 11:26:13 +0200

    Add update to genre

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -97,10 +97,7 @@ exports.genre_delete_post = asyncHandler(async (req, res, next) => {
         Book.find({ genre: req.params.id }),
     ]);
 
-    if (genre === null) {
-        res.redirect("/catalog/genres");
-        return;
-    }
+    if (genre === null) return res.redirect("/catalog/genres");
 
     if (booksFromGenre.length > 0) {
         res.render("genre_delete", {
@@ -116,10 +113,41 @@ exports.genre_delete_post = asyncHandler(async (req, res, next) => {
 
 // Display Genre update form on GET.
 exports.genre_update_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Genre update GET");
+    const genre = await Genre.findById(req.params.id);
+
+    if (genre === null) {
+        const err = new Error("Genre not found");
+        err.status = 404;
+        return next(err);
+    }
+
+    res.render("genre_form", { title: "Update Genre", genre: genre });
 });
 
 // Handle Genre update on POST.
-exports.genre_update_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Genre update POST");
-});
+exports.genre_update_post = [
+    body("name", "Genre must contain at least 3 characters")
+        .trim()
+        .isLength(3)
+        .escape(),
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+
+        const genre = new Genre({ name: req.body.name, _id: req.params.id });
+
+        if (!errors.isEmpty()) {
+            return res.render("genre_form", {
+                title: "Update Genre",
+                genre: genre,
+                errors: errors.array(),
+            });
+        } else {
+            const thegenre = await Genre.findByIdAndUpdate(
+                req.params.id,
+                genre,
+                {}
+            );
+            res.redirect(thegenre.url);
+        }
+    }),
+];
